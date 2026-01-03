1e7fe827338ad885ce5dc5f012c451a76d9cca91
satya gurjar <satyagurjar405@gmail.com>
2026-01-03 21:07:00 +0530

    add new small project

--- Day2 : jsx,Babel,props/index.js
+++ Day2 : jsx,Babel,props/index.js
@@ -19,28 +19,43 @@
 
 // Rect component : its are function
 
-function App(name){
-  return (
-    <h1>hello {name}</h1>
-  );
-}
+// function App(name){
+//   return (
+//     <h1>hello {name}</h1>
+//   );
+// }
  
-const a = App('india');
+// const a = App('india');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
 // root.render(App())  //1st way of component call
 // root.render(<App/>) //2nd way to call component
 
 // root.render(a)  //3rd way 
+//React.crateElement('ul',null,[React.createElement('ul',null,<li>HTML</li>)])
+//React.crateElement('ul',null,[React.createElement('ul',null,<li>CSS</li>)])
 
-const courses = ['HTML','CSS','JavaScript','React'];
-const element =  (
-  <ul>
-    {
-      courses.map(courses=><li>{courses}</li>)
-    }
-  </ul>
-)
 
-root.render(element)
+// const courses = ['HTML','CSS','JavaScript','React'];
+// const element =  (
+//   <ul>
+//     {
+//       courses.map(courses=><li>{courses}</li>)
+//     }
+//   </ul>
+// )
+
+// function App(props){
+//   return(
+//     <h1>hello india {props.name} {props.age}</h1>
+//   )
+// }
+// const ab = {backgroundColor:'red',color:'white'}
+
+// const element = <App name='india' age={11}></App>
+
+// const elementq =  <h1 id='title' className="first" style={ab}>hello india</h1>
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(elementq)
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Day 1: Today i learn how react are build and how it work. I make my own small re
 
 #Day2: Learn Babel: Bable is a javascript compiler, that conver JSX to Js code.
 **Component**: it's just a finction that's return jsx. There first latter is capital
+**Props**: receve multiple arguments just like parameter
+**fragment**: <></>
